d3f96ad254546291e39de03e6731d888ee24107e
sam <sam@sams-MacBook-Pro.local>
2024-04-16 18:02:17 -0400

    Changes to accounts.py

--- flask-app/src/accounts/accounts.py
+++ flask-app/src/accounts/accounts.py
@@ -130,6 +130,21 @@ def get_retirement_transaction_info():
     return user_response
 
 ########################################################
+# Get information retirement account transaction for an account
+@accounts.route('/retirement_transaction/<int:accountNum>', methods=['GET'])
+def get_retirement_transaction_info():
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT account_num, amount, transaction_type FROM retirement_transaction WHERE account_num = %s')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    userData = cursor.fetchall()
+    for row in userData:
+        json_data.append(dict(zip(row_headers, row)))
+
+    user_response = make_response(jsonify(json_data))
+    user_response.status_code = 200
+    user_response.mimetype = 'application/json'
+    return user_response
 
 # Add information of a trade that took place, reflecting the transaction that occurred
 @accounts.route('/trades', methods=['POST'])
